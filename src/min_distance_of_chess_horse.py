dffe600d834bc0bf7f786a5b315418c81d07a95e
cojlogkuu <150828679+cojlogkuu@users.noreply.github.com>
2024-04-16 11:02:21 +0300

    lab 5, opt 2, lvl 3 (#5)
    * lab 5, opt 2, lvl 3
    
    * fix bugs
    
    fix bug with small board
    add test to small board

---
+++ src/min_distance_of_chess_horse.py
@@ -0,0 +1,48 @@
+def get_min_distance_by_bfs(border_size, start_position, end_position):
+    if border_size < 3:
+        return -1
+
+    dist_to = {
+        start_position: 0,
+    }
+    queue = [start_position]
+    visited = set()
+
+    row = [2, 2, -2, -2, 1, 1, -1, -1]
+    col = [-1, 1, 1, -1, 2, -2, 2, -2]
+
+    while queue:
+        vertex = queue.pop(0)
+
+        for k in range(8):
+            neighbor = (vertex[0] + row[k], vertex[1] + col[k])
+
+            if neighbor == end_position:
+                return dist_to[vertex] + 1
+
+            if neighbor in visited or neighbor[0] >= border_size - 1 or neighbor[0] < 0 or neighbor[1] >= border_size - 1 or \
+                    neighbor[1] < 0:
+                continue
+
+            dist_to[neighbor] = dist_to[vertex] + 1
+            queue.append(neighbor)
+            visited.add(neighbor)
+    else:
+        return -1
+
+
+def get_min_distance_of_chess_horse(input_file_name, output_file_name):
+    try:
+        with open(input_file_name, 'r') as file:
+            border_size = int(file.readline())
+            start_position = tuple(map(int, file.readline().split(',')))
+            end_position = tuple(map(int, file.readline().split(',')))
+    except ValueError:
+        with open(output_file_name, 'w') as file:
+            file.write('-1')
+        return
+
+    result = get_min_distance_by_bfs(border_size, start_position, end_position)
+
+    with open(output_file_name, 'w') as file:
+        file.write(str(result))
---
+++ tests/test_min_distance_of_chess_horse.py
@@ -0,0 +1,22 @@
+import unittest
+from src.min_distance_of_chess_horse import get_min_distance_of_chess_horse
+
+class Test(unittest.TestCase):
+    def test_given_input(self):
+        get_min_distance_of_chess_horse('../resources/input', '../resources/output')
+        with open('../resources/output', 'r') as file:
+            result = int(file.readline())
+        self.assertEqual(result, 6)
+
+    def test_wrong_input(self):
+        get_min_distance_of_chess_horse('../resources/wrong_input', '../resources/wrong_output')
+        with open('../resources/wrong_output', 'r') as file:
+            result = int(file.readline())
+        self.assertEqual(result, -1)
+
+    def test_small_board_input(self):
+        get_min_distance_of_chess_horse('../resources/small_board_input', '../resources/small_board_output')
+        with open('../resources/small_board_output', 'r') as file:
+            result = int(file.readline())
+        self.assertEqual(result, -1)
+
