3d0d7799d3c4a2ca7673e06652ddb4b69eaf0d3d
cojlogkuu <150828679+cojlogkuu@users.noreply.github.com>
2024-04-19 20:16:24 +0300

    lab 6, opt 2, lvl 3 (#6)

---
+++ tests/test_min_number_of_beers.py
@@ -0,0 +1,18 @@
+import unittest
+from src.min_number_of_beers import get_min_number_of_beers
+
+
+class Test(unittest.TestCase):
+    def test_two_beers(self):
+        self.assertEqual(get_min_number_of_beers(2, 2, 'NY YN'), 2)
+
+    def test_four_beers(self):
+        self.assertEqual(get_min_number_of_beers(7, 4, 'YNNY YYNN NYYN NNYY YNYY YYNN NYNY'), 2)
+        self.assertEqual(get_min_number_of_beers(7, 4, 'YNNY YYNN NNYN NNYY YNYY YYNN NYNY'), 3)
+
+    def test_three_beers(self):
+        self.assertEqual(get_min_number_of_beers(6, 3, 'YNN YNY YNY NYY NYY NYN'), 2)
+        self.assertEqual(get_min_number_of_beers(6, 3, 'YYN YYY YYY YYY YNY YYY'), 1)
+
+    def test_one_beer(self):
+        self.assertEqual(get_min_number_of_beers(2, 1, 'Y Y'), 1)
